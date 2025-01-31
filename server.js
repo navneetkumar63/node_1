4206c76e13a74726aafcf814db6c015ea99e70bc
navneetkumar63 <158578309+navneetkumar63@users.noreply.github.com>
2025-01-31 14:24:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const passport =require('./auth');
 const Person = require('./models/Person');
 const PORT = process.env.PORT || 3000;
-
+//
 
 app.use(passport.initialize());
 
